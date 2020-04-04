096bf3f66d73dbdb84336fd6849e24c91348c2b0
Daniel Duris <dusoft@gmail.com>
2020-04-04 20:30:45 +0200

    porting of layer fetching and parsing to JS/client from PHP/server

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@
  */
 
 Route::get('/', ['uses' => 'MasterplanController@map', 'as' => 'map']);
-Route::get('/refresh/osm/{force?}', ['uses' => 'MasterplanController@refreshOSMData', 'as' => 'refresh.osm']);
-Route::get('/refresh/eia/{force?}', ['uses' => 'MasterplanController@refreshEIAData', 'as' => 'refresh.eia']);
-Route::get('/refresh/bikeshare/{force?}', ['uses' => 'MasterplanController@refreshBikeshareData', 'as' => 'refresh.bikeshare']);
-Route::get('/refresh/feed/{force?}', ['uses' => 'MasterplanController@refreshFeedData', 'as' => 'refresh.feed']);
+
+Route::prefix('data')->group(function () {
+    Route::get('layer/{id}/{type?}', ['uses' => 'MasterplanController@pushData', 'as' => 'data.layer']);
+});
+
+Route::prefix('refresh')->group(function () {
+    Route::get('osm/{force?}', ['uses' => 'MasterplanController@refreshOSMData', 'as' => 'refresh.osm']);
+    Route::get('eia/{force?}', ['uses' => 'MasterplanController@refreshEIAData', 'as' => 'refresh.eia']);
+    Route::get('bikeshare/{force?}', ['uses' => 'MasterplanController@refreshBikeshareData', 'as' => 'refresh.bikeshare']);
+    Route::get('feed/{force?}', ['uses' => 'MasterplanController@refreshFeedData', 'as' => 'refresh.feed']);
+});
