2f339fdae06dbbe190375b45618c6c01cf393f30
Daniel Duris <dusoft@gmail.com>
2020-04-06 23:14:06 +0200

    data save added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', ['uses' => 'MasterplanController@map', 'as' => 'map']);
 
 Route::prefix('data')->middleware('cache.headers:public;max_age=86400;immutable;etag')->group(function () {
     Route::get('layer/{id}/{type?}', ['uses' => 'MasterplanController@pushData', 'as' => 'data.layer']);
+    Route::post('save', ['uses' => 'MasterplanController@saveData', 'as' => 'data.save']);
 });
 
 Route::prefix('refresh')->group(function () {
