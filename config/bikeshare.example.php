62be1f3259a1bb28f535bc821d7cff21642f9355
dusoft@gmail.com <dusoft@gmail.com>
2021-03-08 22:49:30 +0100

    bikeshare config now able to accept multidimensional arrays

--- config/bikeshare.example.php
+++ config/bikeshare.example.php
@@ -5,9 +5,11 @@ return [
     'bikeshares' => [
         // multiple bike shares supported
         'bikeshare_name' => [
-            'url'           => '', // URL with JSON bikeshare stands (markers)
+            'layer_id'      => 5, // layer id to use (must match layer in config/map.php)
+            'type'          => 1, // type to use (must match layer/type in config/map.php)
+            'url'           => '', // URL with JSON bikeshare stands (markers) or array of multiple URLs
             'name'          => '', // field name in JSON that contains marker "name"
-            'coords'        => ['lat', 'lon'], // marker "coords" = string with lat,lon comma separated field or array of separate lat, lon fields containing coordinates
+            'coords'        => ['lat', 'lon'], // marker "coords" = string with lat,lon comma separated field or array of separate lat, lon fields containing coordinates (dot notation for multidimensional arrays, e.g. points.0.lat or 0.stand.0)
             'description'   => '', // field name in JSON that contains marker "description"
             'filename'      => '', // field name in JSON that contains marker "filename" (e.g. photo)
             'bicycle_count' => '', // field name in JSON that contains number of bicycles at stand (will be added to description)
