5ce352d8b19e0b607a6da497ef6f382a40d13a6b
dusoft@gmail.com <dusoft@gmail.com>
2021-03-08 22:50:10 +0100

    removed hardcoded legacy values, added layer and type

--- config/google.example.php
+++ config/google.example.php
@@ -1,14 +1,13 @@
 <?php
 
 return [
-
     'GTM_ID'             => '', // google tag manager ID
     'APPLICATION_NAME'   => '', // name of your app
     'CLIENT_ID'          => '', // client ID
     'KEY_FILE'           => '', // .json file in public/ directory
     'SPREADSHEETS_SCOPE' => 'https://www.googleapis.com/auth/spreadsheets',
-    'EIA_FILE'           => '', // ID of file
-    'EIA_SHEET'          => '', // Name of sheet to extract data from
-    'SHEET_STRUCTURE'    => ['name' => 0, 'coords' => 1, 'description' => 2, 'cycleways' => 3], // sheet columns to extract data from, 0 = column A; description could be an array, if multiple values should be extracted
-
+    'SPREADSHEET_FILE'   => '', // ID of file
+    'SPREADSHEET_SHEET'  => '', // Name of sheet to extract data from
+    'SHEET_STRUCTURE'    => ['name' => 0, 'coords' => 1, 'description' => 2, 'cycleways' => 3, 'url' => 4], // sheet columns to extract data from, 0 = column A; description could be an array, if multiple values should be extracted
+    'MAP_DETAILS'        => ['layer_id' => 9, 'type' => 1], // layer id and type to save fetched data under
 ];
